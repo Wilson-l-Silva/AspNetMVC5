ccd21ee3813a3793fd0f71faa9bdfcb3cdf80964
wlima <wlima@osas.com.br>
2024-07-28 20:09:58 -0300

    Adicionado visualização de dados usando ViewBag

--- Introducao/Controllers/HomeController.cs
+++ Introducao/Controllers/HomeController.cs
@@ -18,10 +18,22 @@ namespace Introducao.Controllers
                 Tipo = "Pessoa Física"
             };
 
-            //passando informações para view
+            var p2 = new Pessoa
+            {
+                PessoaId = 2,
+                Nome = "Aline Fernandes",
+                Tipo = "Pessoa Física"
+            };
+
+            //passando informações para viewdata
             ViewData["PessoaId"] = p1.PessoaId;
             ViewData["Nome"] = p1.Nome;
             ViewData["Tipo"] = p1.Tipo;
+
+            //passandi informações para view com viewBag
+            ViewBag.id = p2.PessoaId;
+            ViewBag.nome = p2.Nome;
+            ViewBag.tipoPessoa = p2.Tipo;
 
             return View();
         }
